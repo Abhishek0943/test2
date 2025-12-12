f6cdba54b36179df15319ac9303d23121bbbdeff
abhishek0943 <abhishekpundir077@gmail.com>
2025-12-12 16:01:14 +0530

    done

--- src/db.js
+++ src/db.js
@@ -2,10 +2,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const useUrl = !!process.env.DATABASE_URL;
 
 const pool = new Pool(
-  useUrl=  {
+ {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 5432,
         user: process.env.DB_USER || 'modex_user',
